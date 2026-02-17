86940607da1239e66d1153f09b1dceaeb8fcb9a6
Rebecca McShane <rmcshane29@gmail.com>
2026-02-17 15:04:42 +0000

    Have verified vsphere_mine produces a solvable problem

--- vsphere_mine.py
+++ vsphere_mine.py
@@ -34,3 +34,29 @@ dist = d3.Distributor(coords, dtype=PARAMS['dtype'], mesh=mesh)
 ball = d3.BallBasis(coords, shape=(PARAMS['Nphi'], PARAMS['Ntheta'], PARAMS['Nr']),
             radius=PARAMS["radius"], dealias=PARAMS['dealias'], dtype=PARAMS['dtype'])
 sphere = ball.surface
+
+#Field
+u = dist.VectorField(coords, name='u',bases = ball)
+p = dist.Field(name = 'p', bases = ball)
+tau_p = dist.Field(name="tau_p")
+tau_u = dist.VectorField(coords, name="tau_u", bases=sphere)
+
+#Substitutions
+phi, theta, r = dist.local_grids(ball)
+Ek = PARAMS['Ek']
+lift = lambda A: d3.Lift(A, ball, -1)
+
+ang_boundary = dist.VectorField(coords, name = 'spup', bases=sphere)
+ang_boundary['g'][0,:] = PARAMS['radius']*np.sin(theta)
+
+#Problem
+problem = d3.IVP([u, p, tau_p, tau_u], namespace=locals())
+problem.add_equation("div(u) + tau_p = 0")
+problem.add_equation("dt(u) + grad(p) - Ek*lap(u) + lift(tau_u) = -cross(curl(u),u)")
+problem.add_equation("radial(u(r=PARAMS['radius'])) = 0") #Impenetrable boundary condition
+problem.add_equation("angular(u(r=PARAMS['radius'])) = angular(ang_boundary)")
+problem.add_equation("integ(p) = 0") #Pressure gauge
+
+#Solver
+solver = problem.build_solver(PARAMS['timestepper'])
+solver.stop_time = PARAMS["stop_time"]
