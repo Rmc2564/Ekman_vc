1f785e3295b6846b8e33d3972b62333b56b0c26a
Rebecca McShane <rmcshane29@gmail.com>
2026-02-24 18:00:54 +0000

    It works :)

--- viscous_sphere.py
+++ viscous_sphere.py
@@ -68,7 +68,8 @@ omega_R1 = dist.VectorField(coords, bases=ball)(r=radius).evaluate()
 uang_R1['g'][0,:] = (Delta_Omega*sintheta)(r=radius).evaluate()['g']
 omega_R1['g'][2,:] = (Omega_Init + domega)(r=radius).evaluate()['g']
 
-omega_n['g'][2,:] = Omega_Init
+omega_n['g'][1,:] = Omega_Init*-np.sin(theta)
+omega_n['g'][2,:] = Omega_Init*np.cos(theta)
 
 
 lift = lambda A: d3.Lift(A, ball, -1)
