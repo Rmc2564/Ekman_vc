927edf4c0337ee15c76d2dc8086a238169b1f400
Rebecca McShane <rmcshane29@gmail.com>
2026-02-27 15:55:32 +0000

    Added functions from notebook

--- plot_single.py
+++ plot_single.py
@@ -7,6 +7,8 @@ import warnings
 warnings.filterwarnings("ignore")
 
 import scipy.interpolate as inp
+from control_parameters import parameters
+locals().update(parameters)
 
 def my_interp2d(f, rad, radnew):
     r = rad
@@ -18,3 +20,35 @@ def my_interp2d(f, rad, radnew):
         fnew[i, :] = inp.splev(rnew, tckp)
 
     return fnew
+
+def plot_stream(r,theta,vr_n,vtheta_n,density,label=None,clim=[0,0]):
+
+    rad = np.linspace(r[-1], r[0], len(r))
+    theta = np.linspace(0,np.pi,len(theta))
+
+    rr, ttheta = np.meshgrid(rad, theta)
+    plt.figure()
+    fig,ax = plt.subplots(1,1,figsize=(6,6),subplot_kw={'projection': 'polar'})
+
+    un = vr_n[:,::-1]
+    vn = vtheta_n[:,::-1]/rr[:,::-1]
+
+    un = my_interp2d(un, r[::-1], rad)
+    vn = my_interp2d(vn, r[::-1], rad)
+
+    ax.set_theta_zero_location('N')
+    ax.set_theta_direction(-1)
+    ax.set_rorigin(0)
+    ax.set_ylim(r.min(),r.max())
+    ax.set_thetamin(0)
+    ax.set_thetamax(180)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    strm = ax.streamplot(ttheta.T,rr.T,vn.T,un.T,color='#d95f02',density=density,
+                         broken_streamlines=True,linewidth=1)
+
+    fig.tight_layout()
+
+print(Delta_Omega)
